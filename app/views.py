2889b96cb80e4436d866ba6930562164d487c4e7
Bjørn Arild Lunde <bjolun5@gmail.com>
2019-06-03 11:33:21 +0200

    Added and started writing in index.html. Added it to views.py.

--- app/views.py
+++ app/views.py
@@ -5,4 +5,8 @@ from flask import Flask, render_template
 @app.route('/')
 def home():
 
+	return render_template("index.html")
+
+@app.route("/menu")
+def menu():
 	return render_template("menu.html")
