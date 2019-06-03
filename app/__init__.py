c210176c0b00b08c917c29d50c251cc23fbd2e93
Emil Gabrielli <emilgabrielli@gmail.com>
2019-06-03 12:03:47 +0200

    Added database

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,20 @@
+import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 
 # NOT A SECRET KEY ! :(
 app.config['SECRET_KEY'] = 'dev_key'
+
+# configurating the database
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir,'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
+
+# Passes app through SQLAlchemy
+db = SQLAlchemy(app)
+Migrate(app,db)
 
 from app import views
