04481dc59344a63bd11d37fc55f70e18658972d2
Emil Gabrielli <emilgabrielli@gmail.com>
2019-06-11 13:32:03 +0200

    Added interface for adding items to menu

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 from app import app
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, session, redirect, url_for
 from app.models import db, PizzaMenu, ThaiMenu, GrillMenu
+from app.forms import AddPizza, AddThai, AddGrill
 
 
 @app.route('/')
@@ -8,21 +9,42 @@ def home():
 
 	return render_template("index.html")
 
-@app.route("/menu")
-def menu():
-	return render_template("menu.html")
-
-@app.route('/addpizza')
+@app.route('/addpizza',methods=['GET','POST'])
 def addpizza():
 
-	return render_template('forms.html')
+	title = "Legg til pizza"
 
-@app.route('/addthaimat')
+	form = AddPizza()
+
+	if form.validate_on_submit():
+		name = form.name.data
+		session['item'] = form.name.data
+		description = form.description.data
+		allergies = form.allergies.data
+		price = form.price.data
+		return redirect(url_for('add_confirm'))
+
+	return render_template('forms.html', form=form, title=title)
+
+@app.route('/addthaimat',methods=['GET','POST'])
 def addthai():
 
-	return render_template('forms.html')
+	title = "Legg til thaimat"
 
-@app.route('/addgrillmat')
+	form = AddThai()
+
+	return render_template('forms.html', form=form, title=title)
+
+@app.route('/addgrillmat',methods=['GET','POST'])
 def addgrill():
 
-	return render_template('forms.html')
+	title = "Legg til grillmat"
+
+	form = AddGrill()
+
+	return render_template('forms.html', form=form, title=title)
+
+@app.route('/add_confirm', methods=['GET','POST'])
+def add_confirm():
+
+	return render_template('confirm.html')
